c6042e17ce5820ab391784ec789624879021731c
Rafael <dalacquar@gmail.com>
2023-07-25 16:04:37 -0300

    Initial commit

---
+++ src/routes/Home.tsx
@@ -0,0 +1,17 @@
+import { Text, Box } from '@chakra-ui/react';
+import React from 'react';
+
+const Home = () => {
+    return (
+        <>
+            <Box className="apresentation-header" bgColor={"black"} color="white">
+                Conteudo de apresentação:
+            </Box>
+            <Box className="main" bgColor={"black"}>
+                Conteudo de apresentação:
+            </Box>
+        </>
+    )
+}
+
+export default Home;
